32a5659fdc8abd8d6b27ae90e39f64b5795ba135
khurram-works <rajpootkhurramx82@gmail.com>
2026-06-11 22:02:31 +0500

    Working on Chatbot Server sent events

--- src/routes/publicRouter.ts
+++ src/routes/publicRouter.ts
@@ -19,18 +19,30 @@ publicRouter.get("/:slug", async (req: e.Request, res: e.Response) => {
       select: {
         chatbotName: true,
         welcomeMessage: true,
+        slug: true
       },
     });
 
-    console.log(chatbotInfo)
+    if(!chatbotInfo){
+      return res.status(400).json({
+        success: false,
+        message: "Company not found"
+
+      })
+    }
 
     return res.status(200).json({
       success: true,
       chatbotName: chatbotInfo?.chatbotName,
       welcomeMessage: chatbotInfo?.welcomeMessage,
+      slug: chatbotInfo?.slug,
     });
   } catch (err) {
     console.log("Error In Retreiving Chatbot Info: ", err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error occurred"
+    });
   }
 });
 
